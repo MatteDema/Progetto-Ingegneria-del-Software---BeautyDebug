abebd4bd15d72aa401a40730c95c8aeec898ce1f
Alessia <alessiadex03@gmail.com>
2025-07-14 17:10:19 +0200

    commit alessia

--- src/main/java/entity/GiornoSettimanale.java
+++ src/main/java/entity/GiornoSettimanale.java
@@ -1,4 +1,10 @@
 package entity;
 
+import java.time.LocalTime;
+
 public class GiornoSettimanale {
+    private String giorno;
+    private boolean apertura;
+    private LocalTime orarioPrimoAppuntamento;
+    private LocalTime orarioUltimoAppuntamento;
 }
