48f8028697bed18e901093fecd90a351fbaf64ee
aless <alessandrocorrente1201@gmail.com>
2025-07-14 13:07:16 +0200

    Commit Alessandro

--- src/main/java/database/DatabaseDateUtils.java
+++ src/main/java/database/DatabaseDateUtils.java
@@ -3,10 +3,12 @@ package database;
 import java.time.format.DateTimeFormatter;
 
 public class DatabaseDateUtils {
+    //classe di utility per il package Database perché più DAO utilizzano questi formati
 
     public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     public static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
     public static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss");
+
     private DatabaseDateUtils() {
         // Costruttore privato per evitare istanziazione della classe di utility
     }
