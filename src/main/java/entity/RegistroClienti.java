d818e1986ea4d269b594e0950cbc114ed404ddee
Alessia <alessiadex03@gmail.com>
2025-07-14 17:00:33 +0200

    commit alessia

--- src/main/java/entity/RegistroClienti.java
+++ src/main/java/entity/RegistroClienti.java
@@ -4,12 +4,14 @@ public class RegistroClienti {
     //Classe Singleton
      private RegistroClienti registroClienti=null;
 
+     private RegistroClienti(){}
+
 
     boolean registrazione(String nome,String cognome,String indirizzo,String telefono,String email){}
     private boolean verificaSeEmailGiaPresente(String email){}
     boolean verificaSeUtenteGiaRegistrato(String email){}
     boolean inserisciCliente(String nome,String cognome,String indirizzo,String telefono,String email,String username,String password){}
     void accesso(String email,String password){}
-    static RegistroClienti getRegistroClienti(){}
+    private static RegistroClienti getRegistroClienti(){}
 
 }
