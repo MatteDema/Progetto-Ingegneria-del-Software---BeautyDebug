dc185ee518944b38d3eec8cdc89f5f8b1af5a08a
Alessia <alessiadex03@gmail.com>
2025-07-14 17:28:55 +0200

    commit alessia

--- src/main/java/entity/Trattamento.java
+++ src/main/java/entity/Trattamento.java
@@ -6,7 +6,26 @@ public class Trattamento {
     private int costo;
     private String ripetizionePeriodica="non prevista";
 
-    public Trattamento(String nome,String descrizione,int costo,String ripetizionePeriodica){}
+
+    //Costruttore
+    public Trattamento(String nome,String descrizione,int costo,String ripetizionePeriodica){
+        this.nome=nome;
+        this.descrizione=descrizione;
+        this.costo=costo;
+        this.ripetizionePeriodica=ripetizionePeriodica;
+
+    }
 
      int scriviSuDB(String nome){}
+
+
+    // GETTER E SETTER
+    int getCosto() {return costo;}
+    void setCosto(int costo) {this.costo = costo;}
+    String getDescrizione() {return descrizione;}
+    void setDescrizione(String descrizione) {this.descrizione = descrizione;}
+    String getNome() {return nome;}
+    void setNome(String nome) {this.nome = nome;}
+    String getRipetizionePeriodica() {return ripetizionePeriodica;}
+    void setRipetizionePeriodica(String ripetizionePeriodica) {this.ripetizionePeriodica = ripetizionePeriodica;}
 }
