b74098d8112b2f3a1d96b2d0059f5655a02da4db
SalvatoreDeFi <salv.defilippo@studenti.unina.it>
2025-07-14 20:24:47 +0200

    commit Salvatore

--- src/main/java/entity/Prenotazione.java
+++ src/main/java/entity/Prenotazione.java
@@ -9,13 +9,22 @@ public class Prenotazione {
     private int ID;
     private LocalDateTime data;
     private String stato="attivo";
+    private Trattamento tipologiaTrattamento;
+    private Cliente cliente;
 
     //Costruttore
-    public Prenotazione(int ID, LocalDateTime data, String stato) {
+    public Prenotazione(int ID, LocalDateTime data, String stato, String tipologiaTrattamento, String usernameCliente) {
         this.ID = ID;
         this.data = data;
         this.stato = stato;
+        // recupero Trattamento e Cliente associati alla Prenotazione dal DB, usando i loro costruttori con chiave primaria
+        this.tipologiaTrattamento = new Trattamento(tipologiaTrattamento);
+        this.cliente = new Cliente(usernameCliente);
     }
+
+    // costruttore vuoto
+    public Prenotazione() {}
+
     int scriviSuDB(int id){
         DBPrenotazione p= new DBPrenotazione(); //DAO
         p.setData(this.data);
@@ -39,4 +48,16 @@ public class Prenotazione {
     void setID(int ID) {this.ID = ID;}
     String getStato() {return stato;}
     void setStato(String stato) {this.stato = stato;}
+    void setTipologiaTrattamento(String tipologiaTrattamento) {
+        this.tipologiaTrattamento = new Trattamento(tipologiaTrattamento);
+    }
+    Trattamento getTipologiaTrattamento() {
+        return this.tipologiaTrattamento;
+    }
+    void setCliente(String usernameCliente) {
+        this.cliente = new Cliente(usernameCliente);
+    }
+    Cliente getCliente() {
+        return this.cliente;
+    }
 }
