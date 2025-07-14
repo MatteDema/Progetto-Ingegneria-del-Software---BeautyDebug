ddfdf60e3cacf5d4c9786a37071d62a77be708eb
SalvatoreDeFi <salv.defilippo@studenti.unina.it>
2025-07-14 20:54:54 +0200

    commit Salvatore

--- src/main/java/entity/Agenda.java
+++ src/main/java/entity/Agenda.java
@@ -12,11 +12,15 @@ public class Agenda {
     // classe Singleton: in tutta l'applicazione avremo una sola istanza di Agenda
 
     private static Agenda agenda; // attributo statico che rappresenta l'unica istanza della classe Agenda
-    private ArrayList<LocalDateTime> fasceOrarieLibere;
-    private ArrayList<LocalDateTime> slotOccupati;
+    private static ArrayList<LocalDateTime> fasceOrarieLibere;
+    private static ArrayList<Prenotazione> slotOccupati;
 
     // costruttore privato
-    private Agenda() {}
+    private Agenda() {
+        // inizializzazione Array List
+        fasceOrarieLibere = new ArrayList<>();
+        slotOccupati = new ArrayList<>();
+    }
 
     // metodo statico (con visibilità di package) che nasconde la creazione dell'unica istanza di Agenda
     static Agenda getAgenda() {
@@ -26,41 +30,80 @@ public class Agenda {
         return agenda;
     }
 
+    // metodo di utilità per caricare la lista di disponibilità orarie dal DB
+    private static ArrayList<LocalDateTime> getFasceOrarieLavorative() {
+
+        // istanzio un DAO DBFasceOrarieLavorative
+        DBFasceOrarieLavorative DBfasceOrarieLavorative = new DBFasceOrarieLavorative();
+        // uso il DAO per recuperare l'elenco disponibilità orarie dal DB
+        ArrayList<DBFasceOrarieLavorative> DBfasceOrarieComplessive = DBfasceOrarieLavorative.caricaElencoDisponibilitaDaDB();
+
+
+        // costruisco la lista di LocalDateTime da restituire, iterando sulla lista di oggetti DBFasceOrarieLavorative
+        ArrayList<LocalDateTime> fasceOrarieComplessive = new ArrayList<>();
+
+        for (int i = 0; i < DBfasceOrarieComplessive.size(); i++) {
+            fasceOrarieComplessive.add(DBfasceOrarieComplessive.get(i).getDisponibilitaOraria());
+        }
+
+        return fasceOrarieComplessive; // restituisce la lista ottenuta
+    }
+
+    // metodo di utilità per caricare le fasce orarie occupate dal DB
+    private static void getSlotOccupati() {
+
+        // istanzio un DAO DBPrenotazione
+        DBPrenotazione dbPrenotazione = new DBPrenotazione();
+        // uso il DAO per recuperare la lista di prenotazioni attive dal DB
+        ArrayList<DBPrenotazione> DBprenotazioniAttive = dbPrenotazione.caricaListaPrenotazioniAttiveDaDB();
+
+
+        // costruisco la lista di oggetti Entity Prenotazione corrispondenti alle prenotazioni attive di livello DAO,
+        // iterando sulla lista DBPrenotazioniAttive
+        ArrayList<Prenotazione> prenotazioniAttive = new ArrayList<>();
+        for (int i = 0; i < DBprenotazioniAttive.size(); i++) {
+
+            // creo una Prenotazione di appoggio
+            Prenotazione prenotazione = new Prenotazione();
+            prenotazione.setID(DBprenotazioniAttive.get(i).getID());
+            prenotazione.setData(DBprenotazioniAttive.get(i).getData());
+            prenotazione.setStato(DBprenotazioniAttive.get(i).getStato());
+            prenotazione.setTipologiaTrattamento(DBprenotazioniAttive.get(i).getTipologiaTrattamento());
+            prenotazione.setCliente(DBprenotazioniAttive.get(i).getUsernameCliente());
+
+            // aggiungo la prenotazione alla lista di prenotazioni attive
+            prenotazioniAttive.add(prenotazione);
+        }
+
+        slotOccupati = prenotazioniAttive;
+    }
 
     ArrayList<LocalDateTime> cercaFasceOrarieLibere() {
 
         // recupera tutte le disponibilità orarie del centro estetico (fasce orarie prenotate e non)
-        DBFasceOrarieLavorative dbfasceOrarieLavorative = new DBFasceOrarieLavorative();
-
-        ArrayList<DBFasceOrarieLavorative> fasce_orarie_complessive = dbfasceOrarieLavorative.caricaElencoDisponibilitaDaDB();
+        ArrayList<LocalDateTime> fasce_orarie_complessive = getFasceOrarieLavorative();
 
         // recupera le fasce orarie occupate (legate alle prenotazioni attive)
-        DBPrenotazione dbPrenotazione = new DBPrenotazione();
-
-        ArrayList<DBPrenotazione> prenotazioni_attive = dbPrenotazione.caricaListaPrenotazioniAttiveDaDB();
-
-        boolean aggiunta_nuova_fascia_libera = false;
+        // recupera le prenotazioni attive
+        getSlotOccupati();
 
         // inizializzo le fasce orarie libere con tutte le disponibilità orarie del centro estetico
         for (int i = 0; i < fasce_orarie_complessive.size(); i++) {
-            fasceOrarieLibere.add(fasce_orarie_complessive.get(i).getDisponibilitaOraria());
+            fasceOrarieLibere.add(fasce_orarie_complessive.get(i));
         }
 
-        for (int i = 0; i < fasce_orarie_complessive.size(); i++) {
+        // for per aggiornare le fasce orarie libere, sottraendo alle disponibilità orarie complessive
+        // le fasce orarie già occupate
 
-            for (int j = 0; j < prenotazioni_attive.size(); j++) {
-
-                // se una certa fascia oraria è stata prenotata, la rimuovo dalla lista di fasce orarie libere
-                if(prenotazioni_attive.get(j).getData().format(DatabaseDateUtils.DATE_TIME_FORMATTER).equals(fasce_orarie_complessive.get((i)))) {
-                    fasceOrarieLibere.remove()
-                }
-
-            }
+        // elimino le fasce orarie corrispondenti agli slot occupati dall'elenco delle fasce orarie libere
+        // per farlo, itero sulle prenotazioni attive (variabile statica slotOccupati), e ne prendo le fasce orarie corrispondenti
+        for (int i = 0; i < slotOccupati.size(); i++) {
+            fasceOrarieLibere.remove(slotOccupati.get(i).getData());
         }
 
+        // restiruisco le fasce orarie libere
+        return fasceOrarieLibere;
     }
-
-
 
 
 }
