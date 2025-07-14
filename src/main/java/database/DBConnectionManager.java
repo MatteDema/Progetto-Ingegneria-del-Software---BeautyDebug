cfef84c1ced505866f63a7bf94498b579e4f2738
SalvatoreDeFi <salv.defilippo@studenti.unina.it>
2025-07-14 11:57:15 +0200

    commit

---
+++ src/main/java/database/DBConnectionManager.java
@@ -0,0 +1,70 @@
+package database;
+
+import java.sql.*;
+
+public class DBConnectionManager {
+
+    public static String url = "jdbc:mysql://localhost:3306/";
+    public static String dbName = "dbCentroEstetico";
+    public static String driver = "com.mysql.cj.jdbc.Driver";
+    public static String userName = "root";
+    public static String password = ""; // ognuno mette la propria password di MySQL
+
+
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+
+        //1. configurare il Driver Manager
+        Connection conn = null;
+        Class.forName(driver);
+
+        //2. creazione della connessione tramite il Driver Manager
+        conn = DriverManager.getConnection(url+dbName, userName, password);
+
+        //3. restituisce la connessione
+        return conn;
+    }
+
+    public static void closeConnection(Connection c) throws SQLException {
+
+        // chiusura della connessione
+        c.close();
+    }
+
+
+
+    public static ResultSet selectQuery(String query) throws ClassNotFoundException, SQLException {
+
+        // creazione della connessione
+        Connection conn = getConnection();
+
+        // creazione dello statement
+        Statement statement = conn.createStatement();
+
+        // eseguo la query di select che ho fornito come input
+        ResultSet ret = statement.executeQuery(query);
+
+        // chiudo la connessione
+        conn.close();
+
+        //ritorna il Result Set (risultato della query di SELECT, ovvero le tuple prelevate dalle tabelle del database)
+        return ret;
+    }
+
+    public static int updateQuery(String query) throws ClassNotFoundException, SQLException {
+
+        // creazione della connessione
+        Connection conn = getConnection();
+
+        // creazione dello statement
+        Statement statement = conn.createStatement();
+
+        // eseguo la query di update/insert/delete che ho fornito come input
+        int ret = statement.executeUpdate(query);
+
+        // chiudo la connessione
+        conn.close();
+
+        return ret;
+    }
+
+}
