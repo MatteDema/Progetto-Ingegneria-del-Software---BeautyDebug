d6b5d6af47e0775fe4d4ab757c338d7aade9883e
SalvatoreDeFi <salv.defilippo@studenti.unina.it>
2025-07-14 16:32:00 +0200

    commit Salvatore

--- src/main/java/database/DBFasceOrarieLavorative.java
+++ src/main/java/database/DBFasceOrarieLavorative.java
@@ -1,6 +1,52 @@
 package database;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class DBFasceOrarieLavorative {
 
+    private LocalDateTime disponibilitaOraria;
 
+    public ArrayList<DBFasceOrarieLavorative> caricaElencoDisponibilitaDaDB() {
+
+        // lista di appoggio di oggetti di tipo DBFasceOrarieLavorative
+        ArrayList<DBFasceOrarieLavorative> fasce_orarie = new ArrayList<>();
+
+        // definisco la query
+        String query = "SELECT * FROM FasceOrarieLavorative";
+
+        System.out.println(query); // stampa di debug della query
+
+        try {
+
+            // faccio la query di SELECT sfruttando il DBConnectionManager
+            ResultSet rs = DBConnectionManager.selectQuery(query);
+
+            while(rs.next()) {
+
+                // itero sul ResultSet della query
+                // finché ho un risultato, prendo i valori dell'unica colonna disponibilitaOraria della tabella
+                // FasceOrarieLavorative, e li uso per valorizzare l'attributo disponibilitaOraria di ciascun DAO
+                // che verrà poi aggiunto alla lista di appoggio
+
+                // creo un DAO di tipo DBFasceOrarieLavorative
+                DBFasceOrarieLavorative fascia_oraria = new DBFasceOrarieLavorative();
+
+                fascia_oraria.setDisponibilitaOraria(rs.getObject("disponibilitaOraria", LocalDateTime.class));
+
+                // aggiungo la nuova fascia oraria alla lista che devo restituire
+                fasce_orarie.add(fascia_oraria);
+            }
+        } catch(ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+        return fasce_orarie;
+    }
+
+    public void setDisponibilitaOraria(LocalDateTime disponibilitaOraria) {
+        this.disponibilitaOraria = disponibilitaOraria;
+    }
 }
