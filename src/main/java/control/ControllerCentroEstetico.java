2e36f755e742ed5211097b1f10ce8cb55d50b895
SalvatoreDeFi <salv.defilippo@studenti.unina.it>
2025-07-15 20:36:50 +0200

    commit Salvatore

--- src/main/java/control/ControllerCentroEstetico.java
+++ src/main/java/control/ControllerCentroEstetico.java
@@ -6,64 +6,68 @@ import entity.CentroEstetico;
 import java.util.ArrayList;
 
 public class ControllerCentroEstetico {
-    //classe singleton
-    private static ControllerCentroEstetico controller_centro_Estetico;
+    // classe Singleton: in tutta l'applicazione avremo una sola istanza di ControllerCentroEstetico
+    private static ControllerCentroEstetico controller_centro_Estetico; // attributo statico che rappresenta l'unica istanza della classe ControllerCentroEstetico
 
-    //metodo per ottenere l'istanza di ControllerCentroEstetico
+    // metodo statico che nasconde la creazione dell'unica istanza di ControllerCentroEstetico
     public static ControllerCentroEstetico getControllerCentroEstetico(){
         if(controller_centro_Estetico == null){
             controller_centro_Estetico = new ControllerCentroEstetico();
         }
         return controller_centro_Estetico;
     }
-    private ControllerCentroEstetico(){} //costruttore privato
 
-    public boolean aggiungiTrattamento(String nome,String descrizione,int costo,String ripetizionePeriodica){
-        //Ottengo l'instanza della classe singleton centroEstetico
+    private ControllerCentroEstetico(){} // costruttore privato
+
+    public boolean aggiungiTrattamento(String nome, String descrizione, int costo, String ripetizionePeriodica){
+        // Ottengo l'istanza unica della classe Singleton CentroEstetico
         CentroEstetico centroEsteticoEntity= CentroEstetico.getCentroEstetico();
-        //Delego alla classe Entity CentroEstetico l'aggiunta di un nuovo trattamento
-       boolean esito_aggiunta= centroEsteticoEntity.aggiungiTrattamento(nome,descrizione,costo,ripetizionePeriodica);
 
-       return esito_aggiunta;
+        // Delego alla classe Entity CentroEstetico l'aggiunta di un nuovo trattamento
+        boolean esito_aggiunta= centroEsteticoEntity.aggiungiTrattamento(nome, descrizione, costo, ripetizionePeriodica);
+
+        return esito_aggiunta;
     }
+
     public ArrayList<DTOTrattamento> visualizzaTuttiTrattamentiDisponibili(){
-        //Ottengo l'instanza della classe singleton centroEstetico
+        // Ottengo l'istanza della classe Singleton CentroEstetico
         CentroEstetico centroEsteticoEntity= CentroEstetico.getCentroEstetico();
-        //Recupero la lista di tutti i trattamenti disponibili da visualizzare dal livello Entity
-       ArrayList<DTOTrattamento> lista_dei_trattamenti= centroEsteticoEntity.visualizzaTuttiTrattamentiDisponibili();
 
-       return lista_dei_trattamenti;
+        // Recupero dal livello Entity la lista di tutti i trattamenti disponibili che si intende visualizzare
+        ArrayList<DTOTrattamento> lista_dei_trattamenti= centroEsteticoEntity.visualizzaTuttiTrattamentiDisponibili();
 
+        return lista_dei_trattamenti;
     }
+
     public ArrayList<DTOTrattamento> visualizzaTrattamentiPerNome(String nomeTrattamento){
-        //Ottengo l'instanza della classe singleton centroEstetico
+        // Ottengo l'istanza della classe Singleton CentroEstetico
         CentroEstetico centroEsteticoEntity= CentroEstetico.getCentroEstetico();
-        //Recupero il trattamento da visualizzare dal livello Entity
+
+        // Recupero dal livello Entity il trattamento, avente il nome passato come parametro, che si intende visualizzare
         ArrayList<DTOTrattamento> trattamentoPerNome= centroEsteticoEntity.visualizzaTrattamentoPerNome(nomeTrattamento);
 
         return trattamentoPerNome;
     }
+
     public ArrayList<DTOTrattamento> visualizzaTrattamentiPerCosto(int costo){
-        //Ottengo l'instanza della classe singleton centroEstetico
+        // Ottengo l'istanza della classe Singleton CentroEstetico
         CentroEstetico centroEsteticoEntity= CentroEstetico.getCentroEstetico();
-        //Recupero la lista dei trattamenti per costo da visualizzare dal livello Entity
+
+        // Recupero dal livello Entity la lista dei trattamenti, filtrati per costo, che si intende visualizzare
         ArrayList<DTOTrattamento> trattamentiPerCosto= centroEsteticoEntity.visualizzaTrattamentiPerCosto(costo);
 
         return trattamentiPerCosto;
-
     }
 
 
-
-   // Di seguito i metodi che non implementeremo:
-   /* public boolean rimuoviMetodo(){}
-      public boolean modificaTrattamento(){}
-      public void definisciDisponibilitaOrarie(){}
-      public float visualizzaIncassoGiornaliero(){}
-      public ArrayList<DTOSeduta> consultaSedutaPerData(LocalDate data){}
+    // Di seguito i metodi che non implementeremo:
+    /*
+    public boolean rimuoviTrattamento(String nomeTrattamento){}
+    public boolean modificaTrattamento(String nomeTrattamento){}
+    public boolean definisciDisponibilitaOrarie(){}
+    public float visualizzaIncassoGiornaliero(LocalDate data){}
+    public ArrayList<DTOSeduta> consultaSedutaPerData(LocalDate data){}
     */
-
-
 
 
 }
--- src/main/java/control/ControllerGestioneSedute.java
+++ src/main/java/control/ControllerGestioneSedute.java
@@ -1,25 +1,26 @@
 package control;
 
 public class ControllerGestioneSedute {
-    //classe Singleton
-    private  static ControllerGestioneSedute controller_sedute;
+    // classe Singleton: in tutta l'applicazione avremo una sola istanza di ControllerGestioneSedute
+    private  static ControllerGestioneSedute controller_sedute; // attributo statico che rappresenta l'unica istanza della classe ControllerGestioneSedute
 
-    //metodo per ottenere l'istanza di ControllerGestioneSedute
+    // metodo statico che nasconde la creazione dell'unica istanza di ControllerGestioneSedute
     public static ControllerGestioneSedute getControllerGestioneSedute(){
         if(controller_sedute == null){
             controller_sedute = new ControllerGestioneSedute();
         }
         return controller_sedute;
     }
-    private ControllerGestioneSedute(){} //costruttore privato
+
+    private ControllerGestioneSedute(){} // costruttore privato
 
 
     //Di seguito i metodi che non implementeremo:
     /*
-     public void registraDatiSeduta(){}
-     public boolean inviaNotificaNuovoAppuntamento(){}
-     public ArrayList<DTOTrattamento> consultaStoricoTrattamenti(){}
-     */
+    public boolean registraDatiSeduta(String noteCliente, String prodottiUtilizzati, float costoEffettivo, LocalDate dataConsigliata){}
+    public boolean inviaNotificaNuovoAppuntamento(){}
+    public ArrayList<DTOSeduta> consultaStoricoTrattamenti(){}
+    */
 
 
 }
