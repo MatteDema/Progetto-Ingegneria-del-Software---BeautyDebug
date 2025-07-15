c4e4771567d500f5d333e4036bf57e7183d088a8
aless <alessandrocorrente1201@gmail.com>
2025-07-15 18:20:02 +0200

    Commit Alessandro

--- src/main/java/entity/Trattamento.java
+++ src/main/java/entity/Trattamento.java
@@ -8,7 +8,7 @@ public class Trattamento {
     private int costo;
     private String ripetizionePeriodica="non prevista";
 
-    // costruttore con chiave primaria per la lettura dal database di un trattamento
+    // Costruttore con chiave primaria per la lettura dal database di un trattamento
     Trattamento(String nome) {
         // chiama il costruttore con chiave primaria della classe DAO associata
         DBTrattamento dbTrattamento = new DBTrattamento(nome);
@@ -19,7 +19,7 @@ public class Trattamento {
         this.ripetizionePeriodica = dbTrattamento.getRipetizionePeriodica();
     }
 
-    //Costruttore
+    // Costruttore
     public Trattamento(String nome,String descrizione,int costo,String ripetizionePeriodica){
         this.nome=nome;
         this.descrizione=descrizione;
@@ -42,12 +42,36 @@ public class Trattamento {
     }
 
     // GETTER E SETTER
-    int getCosto() {return costo;}
-    void setCosto(int costo) {this.costo = costo;}
-    String getDescrizione() {return descrizione;}
-    void setDescrizione(String descrizione) {this.descrizione = descrizione;}
-    String getNome() {return nome;}
-    void setNome(String nome) {this.nome = nome;}
-    String getRipetizionePeriodica() {return ripetizionePeriodica;}
-    void setRipetizionePeriodica(String ripetizionePeriodica) {this.ripetizionePeriodica = ripetizionePeriodica;}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public int getCosto() {
+        return costo;
+    }
+
+    public void setCosto(int costo) {
+        this.costo = costo;
+    }
+
+    public String getRipetizionePeriodica() {
+        return ripetizionePeriodica;
+    }
+
+    public void setRipetizionePeriodica(String ripetizionePeriodica) {
+        this.ripetizionePeriodica = ripetizionePeriodica;
+    }
 }
