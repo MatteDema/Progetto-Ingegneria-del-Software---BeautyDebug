f88f6ce2d087df9011b7fecb83a97a5116e04f53
SalvatoreDeFi <salv.defilippo@studenti.unina.it>
2025-07-17 00:19:00 +0200

    commit Salvatore

--- src/main/java/database/DBConnectionManager.java
+++ src/main/java/database/DBConnectionManager.java
@@ -12,7 +12,7 @@ public class DBConnectionManager {
     public static String dbName = "dbcentroestetico";
     public static String driver = "com.mysql.cj.jdbc.Driver";
     public static String userName = "root";
-    public static String password = "alessandro"; // ognuno mette la propria password di MySQL
+    public static String password = "#DatabaseIS2025"; // ognuno mette la propria password di MySQL
 
 
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
