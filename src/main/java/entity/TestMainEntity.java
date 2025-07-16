59daf01a3bcac25d16ae1fd1edbe21729c66cb1f
aless <alessandrocorrente1201@gmail.com>
2025-07-16 16:27:02 +0200

    Commit Alessandro

---
+++ src/main/java/entity/TestMainEntity.java
@@ -0,0 +1,40 @@
+package entity;
+
+public class TestMainEntity {
+
+    public static void main(String[] args) {
+
+        // TEST CLASSE ENTITY TRATTAMENTO
+        //TEST 1 SCRIVI SU DB
+//        Trattamento trattamento = new Trattamento("banane", "banane in testa", 5, "");
+//        Trattamento trattamento2 = new Trattamento("pesche", "pesche in testa", 10, "Mensile");
+//
+//        System.out.println(trattamento.scriviSuDB("banane"));
+//        System.out.println(trattamento2.scriviSuDB("pesche"));
+
+//        Trattamento trattamento = new Trattamento("banane");
+//        System.out.println(trattamento);
+
+        //TEST CLASSE ENTITY REGISTRO RegistroClienti
+        //TEST 1 INSERISCI CLIENTE
+//        boolean cliente_aggiunto = RegistroClienti.getRegistroClienti().inserisciCliente("aaaaa","aaaaa","aaaaaa 3","1231231231","alessandro@gmail.com", "daje","DAIperfavore12#");
+//        System.out.println(cliente_aggiunto);
+
+        //TEST 2 VERFICA SE UTENTE GIA REGISTRATO
+//        boolean username_presente = RegistroClienti.getRegistroClienti().verificaSeUtenteGiaRegistrato("ciaosaonoio");
+//        System.out.println("Username: " + username_presente);
+
+        //TEST 3 VERIFICA SE EMAIL GIA PRESENTE
+        //fa il return di emailPresenteInDB già testato
+
+        //TEST 4 REGISTRAZIONE
+//        boolean email_ok = RegistroClienti.getRegistroClienti().registrazione("alessandro@gmail.com");
+//        System.out.println(email_ok);
+
+
+        //TEST PRENOTAZIONE ENTITY
+        //TEST 1 SCRIVI SU DB
+        Prenotazione prenotazione = new Prenotazione();
+
+    }
+}
